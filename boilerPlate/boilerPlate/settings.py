4ba66d22a51afa33a913a4e78065945c83c72ac2
W3NDO <wendonyang@gmail.com>
2020-08-02 07:58:45 +0000

    fixed some issues

--- boilerPlate/boilerPlate/settings.py
+++ boilerPlate/boilerPlate/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-^16(#^lfl5)nra&ueksz(m^+a&1xx2gp*o2-#)uuylyg@x*hw'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
